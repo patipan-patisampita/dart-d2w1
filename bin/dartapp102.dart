d8b03c30b1a808d690c552edf74aab4cf14913db
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-04 16:16:45 +0700

    initial update

---
+++ bin/dartapp102.dart
@@ -0,0 +1,7 @@
+void main(){
+  //Rectangle
+  double length = 50;
+  double width = 40;
+
+  print('Area of Rectangle is $length x $width = ${length * width} ');
+}
---
+++ bin/hello.dart
@@ -0,0 +1,6 @@
+void main() {
+  var name = "Jhon";
+  var age = 25;
+  print("I am" + name);
+  print(" I am $name and $age years old");
+}
